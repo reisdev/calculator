f0cfb61f833768c3ff4b0f0cc7b3d5e0b2119833
mthrsj <matheusdrdj@gmail.com>
2019-04-03 11:46:02 -0300

    Basic calculator init

--- app/src/main/java/com/matheus/calculator/MainActivity.kt
+++ app/src/main/java/com/matheus/calculator/MainActivity.kt
@@ -2,11 +2,14 @@ package com.matheus.calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text1.setText(R.string.value1)
+        text2.setText(R.string.value2)
     }
 }
